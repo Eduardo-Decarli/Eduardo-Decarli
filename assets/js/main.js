bba33f991e5aff4257c551a25ed1304be70e41db
Eduardo Decarli <decarliedu@gmail.com>
2024-12-15 16:14:40 -0300

    feat: finish contats area

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-/**
-* Template Name: Shuffle
-* Template URL: https://bootstrapmade.com/bootstrap-3-one-page-template-free-shuffle/
-* Updated: Aug 07 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 (function() {
   "use strict";
 
